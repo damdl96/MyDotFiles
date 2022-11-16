9a36952ae401faa9149875a82a926229df0586f4
Diego Moreno <diegodm96@hotmail.com>
2022-11-16 16:33:49 -0500

    My dotfiles

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,20 @@
+alias telos='cd ~/Documents/telosLabs/Projects'
+alias sl='cd ~/Documents/telosLabs/Projects/saverlife'
+alias DMHD_MAC='cd ~/Volumes/DMHD_MAC'
+alias redis-server='/Users/flying_dodo/Applications/redis-stable/src/redis-server'
+alias ngrok='/Users/flying_dodo/Applications/ngrok'
+alias ls='ls -G'                              # colorize `ls` output
+alias zshreload='source ~/.zshrc'             # reload ZSH
+alias shtop='sudo htop'                       # run `htop` with root rights
+alias grep='grep --color=auto'                # colorize `grep` output
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias less='less -R'
+alias g='git'
+alias rm='rm -i'                              # confirm removal
+alias cp='cp -i'                              # confirm copy
+alias mv='mv -i'                              # confirm move
+alias cal='gcal --starting-day=1'             # print simple calendar for current month
+alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+
